51f75422993140304ecc5eec0d5c839cef858ab9
renkodiranje <renata.tukic@gmail.com>
2022-12-05 19:05:48 +0100

    10

---
+++ 10_IF/script.js
@@ -0,0 +1,14 @@
+let x, y, z;
+x = 3;
+y = 3;
+z = 40;
+console.log( x==y);
+
+if(x == y) 
+{
+    console.log("x i y su jednaki");
+    console.log("jos jedna poruka");
+    console.log(`vrednosti $(x) i $(y) su jednake `);
+}
+
+console.log("komanda posle if naredbe");
--- prvi java/novi.js
+++ prvi java/novi.js
@@ -11,10 +11,77 @@ let minuti_ukupni = 500; //minuti od ponoci
 let protekli_minuti = minuti_ukupni % 60; //minuti
 console.log(protekli_minuti);
 let protekli_sati = (minuti_ukupni - protekli_minuti) / 60;//sati
-console.log(protekli_sati);
+console.log(protekli_sati + ":" + protekli_minuti);
 
-// na casu
-let minOdPonoci = 135;
-let hours = math.flor(minOdPonoci/60);
-let minutes = minOdPonoci % 60;
-console.log(hours + ":" minutes);
+//zadatak 4
+let datum = new Date(); //datum je objekat koji sadrzi info o trenutnom datumu i vremenu
+let mesec = datum.getMonth() + 1;
+
+console.log(datum.getFullYear());
+console.log(datum.getMonth() + "/" + mesec);
+
+let h = datum.getHours();
+let m = datum.getMinutes();
+console.log(h + ":" + m);
+let mop = h * 60 + m;
+console.log(mop);
+// 3. zadatak
+
+var cenaRobe = 553.22;
+var novcanica = 1000;
+var kusur = novcanica - cenaRobe
+console.log(kusur);
+// zadatak 5
+
+
+
+//zadatak 6
+let ukupnoEvra = 500;
+let kursEvroDinar = 117.35;
+console.log("imamo " + ukupnoEvra * kursEvroDinar + " dinara.");
+
+//zadatak 7
+
+ukupnoEvra = 200;
+let dolarDinar = 111.45;
+let dinar = ukupnoEvra * kursEvroDinar;
+let dolar = dinar / dolarDinar;
+console.log("imamo " + dolar + " dolara");
+console.log( "imamo " + dolar * dolarDinar / kursEvroDinar + " evra");
+
+//zadatak 8
+
+let C = 10;
+let F = C * 1.8 + 32;
+
+console.log( "farenhate vrednost:" + F);
+F = 100;
+console.log("celzijusa je:" + ( F - 32) / 1.8);
+
+//zadatak 9 
+//prva konverzija
+C = 25;
+K = C + 273.15;
+console.log("kelvina je:" + K);
+//druga konverzija
+K = 273.15;
+console.log( "celzijusa je:" + (K - 273.15));
+
+//zadatak 5
+
+let dan = datum.getDate();
+let meseci = datum.getMonth() +1;
+let godina = datum.getFullYear();
+console.log(dan + "." + meseci + "." + godina);
+console.log(godina + "/" + meseci + "/" + dan);
+
+//zadatak 6
+let ukupnoEvro = 100;
+kursEvroDinar = 117.27;
+let dobijeniDinari = ukupnoEvro * kursEvroDinar;
+console.log(dobijeniDinari);
+// drugi nacin 6
+dobijeniDinari = 130000;
+ 
+ukupnoEvro =  dobijeniDinari / kursEvroDinar;
+console.log(ukupnoEvro);
